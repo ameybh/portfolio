91f70ce21e3b18d46c808283a38ad6e5b9c2d6a6
Amey Bhavsar <iamamey24@gmail.com>
2024-11-29 03:01:36 +0530

    rename spline login form page dir

--- src/app/showcase/spline-login-formm/page.tsx
+++ src/app/showcase/spline-login-formm/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { LoadingSpinner } from "@/components/loading-spinner";
 import BlurFade from "@/components/magicui/blur-fade";
 import BlurFadeText from "@/components/magicui/blur-fade-text";
 import { Button } from "@/components/ui/button";
@@ -187,32 +188,3 @@ function SplineLoginPage() {
 }
 
 export default SplineLoginPage;
-
-export interface ISVGProps extends React.SVGProps<SVGSVGElement> {
-  size?: number;
-  className?: string;
-}
-
-export const LoadingSpinner = ({
-  size = 24,
-  className,
-  ...props
-}: ISVGProps) => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={size}
-      height={size}
-      {...props}
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className={cn("animate-spin", className)}
-    >
-      <path d="M21 12a9 9 0 1 1-6.219-8.56" />
-    </svg>
-  );
-};
